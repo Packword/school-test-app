fa7b498d8ef1c0e8e5a570123148df8391f002b0
Packword <maksim_zhelnin@mail.ru>
2023-05-30 18:47:49 +0300

    Complete all todo's

--- WebApp/AccountController.cs
+++ WebApp/AccountController.cs
@@ -12,6 +12,7 @@ namespace WebApp
     public class AccountController : Controller
     {
         private readonly IAccountService _accountService;
+        private readonly object lockObj = new object();
 
         public AccountController(IAccountService accountService)
         {
@@ -19,12 +20,12 @@ namespace WebApp
         }
 
         [HttpGet]
-        public async ValueTask<Account> Get()
+        public ValueTask<Account> Get()
         {
             var user = HttpContext.User;
             var claims = user.Claims;
             string userId = claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
-            return await _accountService.LoadOrCreateAsync(userId); /* TODO 3: Get user id from cookie */
+            return _accountService.LoadOrCreateAsync(userId); /* TODO 3: Get user id from cookie */
         }
 
         //TODO 5: Endpoint should works only for users with "Admin" Role
@@ -41,6 +42,7 @@ namespace WebApp
             //Update account in cache, don't bother saving to DB, this is not an objective of this task.
             var account = await Get();
             account.Counter++;
+            _accountService.UpdateInCache(account);
         }
     }
 }
